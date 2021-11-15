1f12fe5ae3abdf105a4d18aa6eb5903a97e62ced
mediazjr <91097193+mediazjr@users.noreply.github.com>
2021-11-15 08:41:10 -0800

    added generate password function with prompt for password length

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase ="abcdefghijklmnopqrstuvwxyz";
-var upppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = "~`!@#$%^&*()-_=+[]{};'|:,./<>?"; 
+
+ 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  if(!password) return
+
   passwordText.value = password;
 
 }
+
+// added variables for each password parameter
+var lowercase ="abcdefghijklmnopqrstuvwxyz";
+var upppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "~`!@#$%^&*()-_=+[]{};'|:,./<>?";
+
+function generatePassword() {
+  var PasswordCharacters = "";
+  var Password = "";
+  var PasswordLength = prompt("Please choose the length of your password (8-128 characters).");
+  if(!PasswordLength) return
+
+
+
+
+}
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

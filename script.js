676f3c450f8f69aecb2910eb0706bdb181a05089
mediazjr <91097193+mediazjr@users.noreply.github.com>
2021-11-15 08:59:13 -0800

    added alert in case password is below 8 characters

--- script.js
+++ script.js
@@ -18,14 +18,25 @@ function writePassword() {
 var lowercase ="abcdefghijklmnopqrstuvwxyz";
 var upppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
-var symbols = "~`!@#$%^&*()-_=+[]{};'|:,./<>?";
+var special = "~`!@#$%^&*()-_=+[]{};'|:,./<>?";
 
+// added generate password funtion with length prompt 
 function generatePassword() {
   var PasswordCharacters = "";
   var Password = "";
   var PasswordLength = prompt("Please choose the length of your password (8-128 characters).");
   if(!PasswordLength) return
 
+// making sure a number was entered in the prompt
+  if(isNaN(PasswordLength)) {
+    alert("Please enter a number only.")
+    return
+  }
+// making sure password is at least 8 characters
+  if(PasswordLength < 8) {
+    alert("Password must be at least 8 char")
+    return
+  }
 
 
 
